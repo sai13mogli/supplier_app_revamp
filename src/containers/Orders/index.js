b882eccdcb3c41c0415a9d5371878c53f4fdcc66
SahilMoglix <sahil.saxena@moglix.com>
2022-02-04 08:54:55 +0530

    [added dropdown component]

--- src/containers/Orders/index.js
+++ src/containers/Orders/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text} from 'react-native';
 import CustomButton from '../../component/common/Button';
+import DropDown from '../../component/common/DropDown';
 import CustomeIcon from '../../component/common/CustomeIcon';
 const OrdersScreen = () => {
+  const [gender, setGender] = useState('');
+
+  const onChange = value => {
+    setGender(value);
+  };
+
   return (
     <>
       <Text style={{fontSize: 16, fontWeight: 'bold', color: '#000'}}>
         OrdersScreen
       </Text>
+
       {/* Exampe for CustomButton Component */}
       <CustomButton
         title={'Hello'}
@@ -18,6 +26,19 @@ const OrdersScreen = () => {
         iconColor={'#fff'}
         iconType={'font-awesome'}
         onPress={() => alert('running')}
+      />
+      {/* End */}
+
+      {/* Example for Dropdown Component */}
+      <DropDown
+        // enabled={false}
+        placeholder={'Gender'}
+        items={[
+          {label: 'Male', value: 'M'},
+          {label: 'Female', value: 'F'},
+        ]}
+        selectedValue={gender}
+        onValueChange={onChange}
       />
       {/* End */}
     </>
