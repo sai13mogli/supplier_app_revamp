9fbaec710507d740e7f9af4857098d6b192cf9be
Moglix <moglix@MOGNOILTM1284.local>
2022-02-28 12:32:35 +0530

    [added dummy notifications]

--- src/redux/sagas/notifications.js
+++ src/redux/sagas/notifications.js
@@ -8,7 +8,7 @@ import {
   markNotificatioReadById,
   deleteNotificationById,
   markBulkNotificationRead,
-  deleteBulkNotification
+  deleteBulkNotification,
 } from '../../services/notifications';
 // actions
 import {
@@ -23,7 +23,26 @@ function* fetchNotification({payload: {page}}) {
     if (error) {
       yield put(failedFetchNotifications(error));
     } else {
-      yield put(fetchedNotifications(page, data.data));
+      yield put(
+        fetchedNotifications(page, {
+          totalPages: 1,
+          dataList: [
+            {
+              id: 1,
+              title: 'test Notif',
+              content: 'test conraedw ewdkfdhewd edwewdun dewdw',
+              readStatus: false,
+            },
+            {
+              id: 1,
+              title: 'test titlke',
+              content: 'd dwe fewf efw we weufewufew f wef ew f ew ff',
+              readStatus: true,
+            },
+          ],
+        }),
+      );
+      // ));
     }
   } catch (error) {
     yield put(failedFetchNotifications(error));
@@ -34,75 +53,49 @@ function* markRead({payload: {id}}) {
   try {
     const {data, error} = yield call(markNotificatioReadById, id);
     if (error) {
-
     } else {
       yield put(fetchNotifications(0));
     }
-  } catch (error) {
-
-  }
+  } catch (error) {}
 }
 
 function* deleteNotification({payload: {id}}) {
   try {
     const {data, error} = yield call(deleteNotificationById, id);
     if (error) {
-
     } else {
       yield put(fetchNotifications(0));
     }
-  } catch (error) {
-
-  }
+  } catch (error) {}
 }
-
 
 function* markBulkRead() {
   try {
     const {data, error} = yield call(markBulkNotificationRead);
     if (error) {
-
     } else {
       yield put(fetchNotifications(0));
     }
-  } catch (error) {
-
-  }
+  } catch (error) {}
 }
-
 
 function* deleteBulk() {
   try {
     const {data, error} = yield call(deleteBulkNotification);
     if (error) {
-
     } else {
       yield put(fetchNotifications(0));
     }
-  } catch (error) {
-
-  }
+  } catch (error) {}
 }
 
 export default fork(function* () {
-  yield takeEvery(
-    NOTIFICATIONS_ACTIONS.FETCH_NOTIFICATIONS,
-    fetchNotification,
-  );
-  yield takeEvery(
-    NOTIFICATIONS_ACTIONS.MARK_READ,
-    markRead,
-  );
+  yield takeEvery(NOTIFICATIONS_ACTIONS.FETCH_NOTIFICATIONS, fetchNotification);
+  yield takeEvery(NOTIFICATIONS_ACTIONS.MARK_READ, markRead);
   yield takeEvery(
     NOTIFICATIONS_ACTIONS.DELETE_NOTIFICATION,
     deleteNotification,
   );
-  yield takeEvery(
-    NOTIFICATIONS_ACTIONS.MARK_BULK_READ,
-    markBulkRead,
-  );
-  yield takeEvery(
-    NOTIFICATIONS_ACTIONS.DELETE_BULK,
-    deleteBulk,
-  );
+  yield takeEvery(NOTIFICATIONS_ACTIONS.MARK_BULK_READ, markBulkRead);
+  yield takeEvery(NOTIFICATIONS_ACTIONS.DELETE_BULK, deleteBulk);
 });
