254ddde39c53382a1a41ff3521d80c6c5f3dd098
Moglix <moglix@MOGNOILTM1284.local>
2022-04-04 12:09:48 +0530

    [more screen routing + setting screen]

--- src/containers/More/index.js
+++ src/containers/More/index.js
@@ -1,113 +1,158 @@
-import React, {useState, useEffect} from 'react';
-import {Text, View, FlatList, ScrollView, TouchableOpacity} from 'react-native';
+import React, {useEffect} from 'react';
+import {Text, View, ScrollView, TouchableOpacity} from 'react-native';
 import {MORE_TABS, PRIVACY_TABS} from '../../constants';
 import styles from './style';
 import CustomeIcon from '../../component/common/CustomeIcon';
 import Dimension from '../../Theme/Dimension';
 import Colors from '../../Theme/Colors';
 import {useDispatch, useSelector} from 'react-redux';
-import {useNavigation} from '@react-navigation/native';
+import {fetchProfile} from '../../redux/actions/profile';
+import {STATE_STATUS} from '../../redux/constants';
+
 const MoreScreen = props => {
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (profileStatus !== STATE_STATUS.FETCHED) {
+      dispatch(fetchProfile());
+    }
+  }, []);
+
   const profileStatus = useSelector(
     state => (state.profileReducer || {}).status || STATE_STATUS.UNFETCHED,
   );
   const profileData = useSelector(state => state.profileReducer.data || {});
-  const navigateFn = () => props.navigation.navigate();
+
+  const getTime = time => {
+    let months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sept',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    let date = new Date(time);
+    return `${months[date.getMonth()]} ${date.getDate()},${date.getFullYear()}`;
+  };
+
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
-      {/* <Text style={{marginTop: 40}}>
-        {(profileData.userInfo || {}).contactName} |{' '}
-        {(profileData.userInfo || {}).email}
-      </Text>
-      <Text style={{marginTop: 10}}>{(profileData.userInfo || {}).phone}</Text>
-      <Text style={{marginTop: 10}}>
-        {(profileData.userInfo || {}).entityName} Since March 2021
-      </Text> */}
-      <View style={{marginTop: 20}}>
-        {MORE_TABS.map((tab, tabIndex) => (
-          <View>
-            <TouchableOpacity
-              key={tabIndex}
-              style={styles.profileTabWrap}
-              onPress={() => props.navigation.navigate(`${tab.route}`)}>
-              <View style={{flexDirection: 'row'}}>
-                <View style={[styles.IconWrap]}>
-                  <CustomeIcon
-                    name={tab.icon}
-                    color={Colors.headerTxtColor}
-                    size={Dimension.font14}></CustomeIcon>
-                </View>
-                <View>
-                  <Text style={styles.tabTitle}>{tab.title}</Text>
-                </View>
-              </View>
-
-              <CustomeIcon
-                name={'arrow-forward'}
-                color={Colors.headerTxtColor}
-                size={Dimension.font18}></CustomeIcon>
-            </TouchableOpacity>
-          </View>
-        ))}
-      </View>
-
-      <View style={{marginTop: 20}}>
-        {PRIVACY_TABS.map((tab, tabIndex) => (
-          <View>
-            <TouchableOpacity
-              key={tabIndex}
-              style={styles.profileTabWrap}
-              onPress={() => props.navigation.navigate(`${tab.route}`)}>
-              <View style={{flexDirection: 'row'}}>
-                <View style={[styles.IconWrap]}>
-                  <CustomeIcon
-                    name={tab.icon}
-                    color={Colors.headerTxtColor}
-                    size={Dimension.font14}></CustomeIcon>
-                </View>
-                <View>
-                  <Text style={styles.tabTitle}>{tab.title}</Text>
-                </View>
-              </View>
-
-              <CustomeIcon
-                name={'arrow-forward'}
-                color={Colors.headerTxtColor}
-                size={Dimension.font18}></CustomeIcon>
-            </TouchableOpacity>
-          </View>
-        ))}
-      </View>
-
-      <View style={styles.varsionWrap}>
-        <View style={{flexDirection: 'row'}}>
-          <CustomeIcon
-            name={'device-mobile'}
-            color={Colors.headerTxtColor}
-            size={Dimension.font18}></CustomeIcon>
-
-          <View
-            style={{flexDirection: 'column', marginTop: -Dimension.margin10}}>
-            <Text
-              style={[styles.versionText, {marginLeft: Dimension.margin10}]}>
-              App Version 1.20
-            </Text>
-            <Text
-              style={[styles.versionText, {marginLeft: Dimension.margin10}]}>
-              Last updated on 12-02-19
-            </Text>
-          </View>
-          <Text
-            numberOfLines={2}
-            style={[
-              styles.versionText,
-              {marginLeft: Dimension.margin70, width: 80, bottom: 5},
-            ]}>
-            No Update Available
+      <ScrollView>
+        <View style={{backgroundColor: Colors.blackColor}}>
+          <Text style={{marginTop: 40}}>
+            {(profileData.userInfo || {}).contactName}
+          </Text>
+          <Text style={{marginTop: 10}}>
+            {(profileData.userInfo || {}).email} |{' '}
+            {(profileData.userInfo || {}).phone}
+          </Text>
+          <Text style={{marginTop: 10}}>
+            {(profileData.userInfo || {}).entityName} Since{' '}
+            {getTime(profileData.createdAt || '')}
           </Text>
         </View>
-      </View>
+        <View style={{marginTop: 20}}>
+          {MORE_TABS.map((tab, tabIndex) => (
+            <View>
+              <TouchableOpacity
+                key={tabIndex}
+                style={styles.profileTabWrap}
+                onPress={() =>
+                  tab.route
+                    ? props.navigation.navigate(`${tab.route}`, {
+                        setIsLoggedIn: props.setIsLoggedIn,
+                      })
+                    : tab.onPress()
+                }>
+                <View style={{flexDirection: 'row'}}>
+                  <View style={[styles.IconWrap]}>
+                    <CustomeIcon
+                      name={tab.icon}
+                      color={Colors.headerTxtColor}
+                      size={Dimension.font14}></CustomeIcon>
+                  </View>
+                  <View>
+                    <Text style={styles.tabTitle}>{tab.title}</Text>
+                  </View>
+                </View>
+
+                <CustomeIcon
+                  name={'arrow-forward'}
+                  color={Colors.headerTxtColor}
+                  size={Dimension.font18}></CustomeIcon>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </View>
+
+        <View style={{marginTop: 20}}>
+          {PRIVACY_TABS.map((tab, tabIndex) => (
+            <View>
+              <TouchableOpacity
+                key={tabIndex}
+                style={styles.profileTabWrap}
+                onPress={() =>
+                  tab.route
+                    ? props.navigation.navigate(`${tab.route}`)
+                    : tab.onPress()
+                }>
+                <View style={{flexDirection: 'row'}}>
+                  <View style={[styles.IconWrap]}>
+                    <CustomeIcon
+                      name={tab.icon}
+                      color={Colors.headerTxtColor}
+                      size={Dimension.font14}></CustomeIcon>
+                  </View>
+                  <View>
+                    <Text style={styles.tabTitle}>{tab.title}</Text>
+                  </View>
+                </View>
+
+                <CustomeIcon
+                  name={'arrow-forward'}
+                  color={Colors.headerTxtColor}
+                  size={Dimension.font18}></CustomeIcon>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </View>
+
+        <View style={styles.varsionWrap}>
+          <View style={{flexDirection: 'row'}}>
+            <CustomeIcon
+              name={'device-mobile'}
+              color={Colors.headerTxtColor}
+              size={Dimension.font18}></CustomeIcon>
+
+            <View
+              style={{flexDirection: 'column', marginTop: -Dimension.margin10}}>
+              <Text
+                style={[styles.versionText, {marginLeft: Dimension.margin10}]}>
+                App Version 1.20
+              </Text>
+              <Text
+                style={[styles.versionText, {marginLeft: Dimension.margin10}]}>
+                Last updated on 12-02-19
+              </Text>
+            </View>
+            <Text
+              numberOfLines={2}
+              style={[
+                styles.versionText,
+                {marginLeft: Dimension.margin70, width: 80, bottom: 5},
+              ]}>
+              No Update Available
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {
   TouchableOpacity,
   View,
@@ -9,8 +9,8 @@ import {
   ActivityIndicator,
 } from 'react-native';
 import Header from '../../component/common/Header';
-import { useNavigation } from '@react-navigation/native';
-import { useDispatch, useSelector } from 'react-redux';
+import {useNavigation} from '@react-navigation/native';
+import {useDispatch, useSelector} from 'react-redux';
 import {
   fetchBusinessDetails,
   fetchProfile,
@@ -19,21 +19,21 @@ import {
   fetchAddressDetails,
   logout,
 } from '../../redux/actions/profile';
-import { fetchCategoriesBrands } from '../../redux/actions/categorybrand';
+import {fetchCategoriesBrands} from '../../redux/actions/categorybrand';
 
 import Progress from 'react-native-progress/Bar';
-import { OrderedMap } from 'immutable';
-import { PROFILE_TABS } from '../../constants';
+import {OrderedMap} from 'immutable';
+import {PROFILE_TABS} from '../../constants';
 import styles from './style';
 import CustomButton from '../../component/common/Button';
 import Colors from '../../Theme/Colors';
 import Dimension from '../../Theme/Dimension';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import CustomeIcon from '../../component/common/CustomeIcon';
-import { STATE_STATUS } from '../../redux/constants';
+import {STATE_STATUS} from '../../redux/constants';
 
 const ProfileScreen = props => {
-  const { navigate } = useNavigation();
+  const {navigate} = useNavigation();
   const dispatch = useDispatch();
   const profileStatus = useSelector(
     state => (state.profileReducer || {}).status || STATE_STATUS.UNFETCHED,
@@ -61,11 +61,11 @@ const ProfileScreen = props => {
 
   useEffect(() => {
     if (profileStatus !== STATE_STATUS.FETCHED) {
+      dispatch(fetchProfile());
       dispatch(fetchAddressDetails());
       dispatch(fetchBusinessDetails());
       dispatch(fetchBankDetails());
       dispatch(fetchTdsInfoDetails());
-      dispatch(fetchProfile());
       dispatch(fetchCategoriesBrands());
     }
   }, []);
@@ -86,19 +86,12 @@ const ProfileScreen = props => {
     }
   };
 
-  const onLogout = async () => {
-    await AsyncStorage.removeItem('token');
-    await AsyncStorage.removeItem('userId');
-    props.setIsLoggedIn(false);
-    dispatch(logout());
-  };
-
   return (
-    <View style={{ flex: 1 }}>
-      <Header showText={'My Profile'} />
+    <View style={{flex: 1}}>
+      <Header showText={'My Profile'} navigation={props.navigation} showBack />
       {profileStatus == STATE_STATUS.FETCHING ? (
         <ActivityIndicator
-          style={{ alignSelf: 'center', margin: Dimension.margin12 }}
+          style={{alignSelf: 'center', margin: Dimension.margin12}}
           size={'small'}
           color={Colors.BrandColor}
         />
@@ -141,7 +134,7 @@ const ProfileScreen = props => {
                     {(profileData.userInfo || {}).email}
                   </Text>
                 </View>
-                <View style={{ flexDirection: 'row' }}>
+                <View style={{flexDirection: 'row'}}>
                   <CustomeIcon
                     name={
                       profileData?.phoneVerified
@@ -157,7 +150,7 @@ const ProfileScreen = props => {
               </View>
             </View>
             <View style={styles.UserEmailVerfyWrap}>
-              <View style={{ flex: 6, marginRight: Dimension.margin10 }}>
+              <View style={{flex: 6, marginRight: Dimension.margin10}}>
                 <Text style={styles.UserEmailVerfyBoldTxt}>
                   A verification link has been sent on your email.
                 </Text>
@@ -165,7 +158,7 @@ const ProfileScreen = props => {
                   Link is active for 24 hours only.
                 </Text>
               </View>
-              <View style={{ flex: 3 }}>
+              <View style={{flex: 3}}>
                 <CustomButton
                   title={'OPEN MAIL'}
                   onPress={() => Linking.openURL('mailto:')}
@@ -206,7 +199,7 @@ const ProfileScreen = props => {
                 key={tabIndex}
                 onPress={() => isActive(tabIndex, tab.route, tab)}
                 style={styles.profileTabWrap}>
-                <View style={{ flexDirection: 'row' }}>
+                <View style={{flexDirection: 'row'}}>
                   <View
                     style={[
                       styles.IconWrap,
@@ -228,7 +221,7 @@ const ProfileScreen = props => {
                   <View>
                     <Text style={styles.tabTitle}>{tab.title}</Text>
 
-                    <View style={{ flexDirection: 'row' }}>
+                    <View style={{flexDirection: 'row'}}>
                       {isCompleted(tab.progress) ? (
                         <>
                           <CustomeIcon
@@ -254,15 +247,6 @@ const ProfileScreen = props => {
                   size={Dimension.font18}></CustomeIcon>
               </TouchableOpacity>
             )).toList()}
-          </View>
-          <View style={styles.logoutBtnWrap}>
-            <TouchableOpacity onPress={onLogout} style={styles.logoutBtn}>
-              <Text style={styles.logoutBtnTxt}>LOGOUT</Text>
-              <CustomeIcon
-                name={'shut-down'}
-                color={Colors.FontColor}
-                size={Dimension.font16}></CustomeIcon>
-            </TouchableOpacity>
           </View>
         </ScrollView>
       )}
