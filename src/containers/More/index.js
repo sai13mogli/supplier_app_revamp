b026960fe7fc29fb2f9f08c393014e73f446c96c
sai13mogli <sai@moglix.com>
2022-04-04 11:55:36 +0530

    [more screen added✅]

--- src/containers/More/index.js
+++ src/containers/More/index.js
@@ -6,24 +6,95 @@ import CustomeIcon from '../../component/common/CustomeIcon';
 import Dimension from '../../Theme/Dimension';
 import Colors from '../../Theme/Colors';
 import {useDispatch, useSelector} from 'react-redux';
-import {useNavigation} from '@react-navigation/native';
+import {
+  fetchBusinessDetails,
+  fetchProfile,
+  fetchBankDetails,
+  fetchTdsInfoDetails,
+  fetchAddressDetails,
+  logout,
+} from '../../redux/actions/profile';
+import {fetchCategoriesBrands} from '../../redux/actions/categorybrand';
+import {STATE_STATUS} from '../../redux/constants';
+import VersionCheck from 'react-native-version-check';
+
 const MoreScreen = props => {
   const dispatch = useDispatch();
   const profileStatus = useSelector(
     state => (state.profileReducer || {}).status || STATE_STATUS.UNFETCHED,
   );
-  const profileData = useSelector(state => state.profileReducer.data || {});
-  const navigateFn = () => props.navigation.navigate();
+  const userInfo = useSelector(
+    state => ((state.profileReducer || {}).data || {}).userInfo || {},
+  );
+
+  const profileData = useSelector(
+    state => (state.profileReducer || {}).data || {},
+  );
+
+  useEffect(() => {
+    if (profileStatus !== STATE_STATUS.FETCHED) {
+      dispatch(fetchAddressDetails());
+      dispatch(fetchBusinessDetails());
+      dispatch(fetchBankDetails());
+      dispatch(fetchTdsInfoDetails());
+      dispatch(fetchProfile());
+      dispatch(fetchCategoriesBrands());
+    }
+  }, []);
+
+  const getTime = time => {
+    let months = {
+      '01': 'January',
+      '02': 'February',
+      '03': 'March',
+      '04': 'April',
+      '05': 'May',
+      '06': 'June',
+      '07': 'July',
+      '08': 'August',
+      '09': 'September',
+      10: 'October',
+      11: 'November',
+      12: 'December',
+    };
+
+    let date = (time || '').split(' ')[0];
+    let month = (date || '').split('-')[1];
+    let year = (date || '').split('-')[0];
+
+    return `Since ${months[month]} ${year}`;
+  };
+
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
-      {/* <Text style={{marginTop: 40}}>
-        {(profileData.userInfo || {}).contactName} |{' '}
-        {(profileData.userInfo || {}).email}
+      <Text
+        style={{
+          marginTop: 40,
+          color: '#000',
+          fontSize: 12,
+          fontWeight: 'bold',
+        }}>
+        {userInfo.contactName}
+        {userInfo.phone} | {userInfo.email}
       </Text>
-      <Text style={{marginTop: 10}}>{(profileData.userInfo || {}).phone}</Text>
-      <Text style={{marginTop: 10}}>
-        {(profileData.userInfo || {}).entityName} Since March 2021
-      </Text> */}
+      <Text
+        style={{
+          marginTop: 10,
+          fontSize: 12,
+          fontWeight: 'bold',
+          color: '#000',
+        }}>
+        Moglix Supplier Since
+      </Text>
+      <Text
+        style={{
+          marginTop: 10,
+          fontSize: 12,
+          fontWeight: 'bold',
+          color: '#000',
+        }}>
+        {getTime(profileData.createdAt)}
+      </Text>
       <View style={{marginTop: 20}}>
         {MORE_TABS.map((tab, tabIndex) => (
           <View>
@@ -91,21 +162,21 @@ const MoreScreen = props => {
             style={{flexDirection: 'column', marginTop: -Dimension.margin10}}>
             <Text
               style={[styles.versionText, {marginLeft: Dimension.margin10}]}>
-              App Version 1.20
+              App Version {VersionCheck.getCurrentVersion()}
             </Text>
-            <Text
+            {/* <Text
               style={[styles.versionText, {marginLeft: Dimension.margin10}]}>
               Last updated on 12-02-19
-            </Text>
+            </Text> */}
           </View>
-          <Text
+          {/* <Text
             numberOfLines={2}
             style={[
               styles.versionText,
               {marginLeft: Dimension.margin70, width: 80, bottom: 5},
             ]}>
             No Update Available
-          </Text>
+          </Text> */}
         </View>
       </View>
     </View>
